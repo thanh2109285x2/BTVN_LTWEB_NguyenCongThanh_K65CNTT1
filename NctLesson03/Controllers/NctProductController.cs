8e9035c8b82a52fc69b178e982b82aa3f3a1ef72
thanh2109285x2 <pnguyencongthanh90@gmail.com>
2026-08-30 17:05:17 +0700

    add-lesson3

---
+++ NctLesson03/Controllers/NctProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NctLesson03.Models;
+
+namespace NctLesson03.Controllers
+{
+    [Route("san-pham")]
+    public class NctProductController : Controller
+    {
+        private List<NctProduct> GetProductList()
+        {
+            return new List<NctProduct>
+            {
+                new NctProduct{Id = 1, Name = "Bộ đồ bơi cho trẻ em nam", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/1.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "Cloth" },
+                new NctProduct{Id = 2, Name = "Bộ đồ bơi cho trẻ em nữ", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/2.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "Cloth" },
+                new NctProduct{Id = 3, Name = "Bộ đồ bơi cho trẻ em từ 3-5 tuổi", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/3.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "Cloth" },
+                new NctProduct{Id = 4, Name = "Bộ đồ bơi cho trẻ em thời trang", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/4.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "Cloth" },
+                new NctProduct{Id = 5, Name = "Túi thời trang mẫu mới 2021", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/5.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "HandBag" },
+                new NctProduct{Id = 6, Name = "Túi thời trang da cá sấu", OldPrice = 50000, NewPrice = 35000, ImageUrl = "/images/6.png", Status = "Còn hàng", CreatedDate = new DateTime(2021, 7, 15, 12, 0, 0), Category = "HandBag" }
+            };
+        }
+        private List<string> GetCategoryList()
+        {
+            return new List<string> { "Cloth", "HandBag", "Watch", "Tv", "Fridge", "Pump", "Fan", "Heater" };
+        }
+        [HttpGet("")]
+        public IActionResult Index(string? category)
+        {
+            var products = GetProductList();
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                products = products.Where(p => p.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            ViewBag.Products = products;
+            ViewBag.Categories = GetCategoryList();
+            return View();
+        }
+        [HttpGet("chi-tiet/id={id}")]
+        public IActionResult Detail(int id)
+        {
+            var products = GetProductList();
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Product = product;
+            return View();
+        }
+    }
+}
---
+++ NctLesson03/Models/NctProduct.cs
@@ -0,0 +1,26 @@
+﻿namespace NctLesson03.Models
+{
+    public class NctProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double OldPrice { get; set; }
+        public double NewPrice { get; set; }
+        public string ImageUrl { get; set; }
+        private string? _description;
+        public string Description
+        {
+            get
+            {
+                return string.IsNullOrEmpty(_description) ? Name : Description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Category { get; set; }
+    }
+}
