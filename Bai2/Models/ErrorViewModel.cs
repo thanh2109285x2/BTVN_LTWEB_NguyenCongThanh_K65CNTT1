5a6cac8cf1c67257fdc0f9ef53bad09735dfdda7
thanh2109285x2 <pnguyencongthanh90@gmail.com>
2026-09-12 23:01:37 +0700

    update work lab3 final

--- Bai2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bai2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
